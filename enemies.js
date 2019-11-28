eaf6bdbc38ed5bc0d4a486241dfea33cc9cc8804
Micah <kanodecat@gmail.com>
2019-11-28 05:00:59 -0500

    Update enemies.js

--- enemies.js
+++ enemies.js
@@ -1,14 +1,21 @@
 var character = {
-    health: 100, mana: 50, gold: 0, state: "idle"
+    health: 100, mana: 100, gold: 0, state: "idle", level: 0
 };
 
-var en1 = new monster("Slime", 5, 1, 10);
-var en2 = new monster("Giant Slime", 10, 5, 20);
+var en1 = new monster("Slime", 2, 1, 5, 1);
+var en2 = new monster("King Slime", 10, 15, 40, 13);
+var en3 = new monster("Metal Slime", 4, 5, 10, 7) ;
+var en4 = new monster("Shadow", 20, 10, 20, 15);
+var en5 = new monster("Lamp", 1, 1, 3, 1);
+var en6 = new monster("Bat", 4, 1, 5, 3);
+var en7 = new monster("goblin", 9, 4, 10, 10);
+var en8 = new monster("android", 15, 20, 50, 20);
+var en9 = new monster("StarMan", 20, 45, 70, 25);
 
-function monster(name, damage, dropamount, health){
-
+function monster(name, damage, dropamount, health, exp){
     this.name = name;
     this.damage = damage;
     this.dropamount = dropamount;
     this.health = health;
+    this.exp = exp;
 }
