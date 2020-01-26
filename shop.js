b0f338e3bfc2d9174826a997af7a512b495c0868
Micah <56785252+kanodecat@users.noreply.github.com>
2020-01-26 13:11:30 -0500

    Update to shop.js

--- shop.js
+++ shop.js
@@ -1,7 +1,15 @@
 function openShop(){
-    
+    setTimeout(() =>{
+        texta.textContent("Welcome to the shop,");
+        texta.textContent("\nwhat would you like to buy?: ")
+    },5000);
+    texta.textContent("");
+    texta.textContent("");
 }
 
+function shopItems(){
+    
+}
 
 
 Console.log(Math.floor(Math.random() * Math.random()));
